e414d42af6b75974629c6eb64de8f2ef5e920582
Aayush Poudel <poudelaayush2006@gmail.com>
2020-04-13 17:50:21 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.awt;
+import java.wt.event;
+
+
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Frame f = new Frame();
+		Label l = new Label("Party at Tim's");
+
 	}
 }
