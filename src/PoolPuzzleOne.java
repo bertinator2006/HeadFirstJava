57b3358305b92eb7a5b5b7c4ce8e438317f15be0
Aayush Poudel <poudelaayush2006@gmail.com>
2020-04-14 21:40:48 +1000

    Update PoolPuzzleOne.java

--- src/PoolPuzzleOne.java
+++ src/PoolPuzzleOne.java
@@ -1,7 +1,25 @@
 class PoolPuzzleOne{
 	public static void main(String[] args) {
-		if (x , 1){
-			System.out.print("Yep!");
+		String a = "a";
+		String noise = "noise";
+		String oyster
+		while (){
+			if (x < 1){
+				System.out.print("a");
+			}
+			System.out.print(" ");
+			if (x > 1){
+
+			}
+			if (x == 1){
+
+			}
+
+			if (){
+
+			}
+			System.out.println("");
+			x = x + 1;
 		}
 	}
 }
